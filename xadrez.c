016a392eebe6b6b3c3902d24c8aaf7fea1296b8e
Carlos-Guedes <gds.g4mer@gmail.com>
2025-08-31 00:36:36 +0000

    f_Recursiva...

--- xadrez.c
+++ xadrez.c
@@ -1,39 +1,57 @@
 #include <stdio.h>
+// Movimentos da Rainha, 8 casas para Esquerda.
+void recursiva(int movRainha){
+    if (movRainha > 0 ){
+        recursiva(movRainha - 1);
+        printf("%d Esquerda\n", movRainha);}}
+
+// Movimentos da Torre, 5 para Direita.
+void recursiva2(int movTorre){
+    if (movTorre > 0){
+        recursiva2(movTorre - 1);
+        printf("%d Direita\n", movTorre);}}
+
+// Movimnentos do Bispo, 5 vertical e 5 horizontal.
+void recursiva3(int movBispo){
+    if (movBispo  > 0){
+        for (int i = 1; i <= movBispo; i++){
+            if (i % 2 == 0){
+                printf("%d Direita\n", i);
+            }else
+                printf("%d Cima\n", i);}}}
+
+
+// Movimentos do Cavalo, 2 para cima e 1 direita.
+void recursiva4(int movCavalo, int movCavalo2){
+    if (movCavalo && movCavalo2 > 0){
+        recursiva4(movCavalo, movCavalo2 - 1);
+        for (movCavalo, movCavalo2  = 1; movCavalo && movCavalo2 <= 1; movCavalo, movCavalo2 ++){
+            int i = 1;
+            while (i <= 2){
+                printf("%d Cima\n", i);
+                i++;
+            }printf("%d Direita\n", i);}}}
 
 int main(){
-
-//RAINHA_8_PRA_ESQUERDA
-    for(int Rainha = 1; Rainha <= 8; Rainha++){
-        printf("%d Esquerda\n", Rainha);
-}printf(" \n");
-    
-//TORRE_5_PRA_DIREITA    
-int torre = 1;
-    while (torre <= 5){
-        printf("%d Direita\n", torre);
-        torre++;
-    }printf(" \n");
-
-//BISPO_5_HORIZONTAL    
-int Bispo = 1;
-    do{
-        printf("%d Cima, Direita\n", Bispo);
-        Bispo++;
-    }while (Bispo <= 5);
+// RAINHA
+    int numMovimentos = 8;
+    recursiva(numMovimentos);
     printf(" \n");
 
-//CAVALO
-int i , j ;
-    for (i = 1; i <= 1; i++){
-        j = 1;
-        while (j <= 2){
-            printf("Baixo\n");
-            j++;
-        }printf("Esquerda\n");
-    }
+// TORRE
+    int numMovimentos2 = 5;
+    recursiva2(numMovimentos2);
+    printf(" \n");
 
+// BISPO
+    int numMovimentos3 = 10;
+    recursiva3(numMovimentos3);
+    printf(" \n");
 
+// CAVALO
+    int numMovimentos4 , num4 = 1;
+    recursiva4(numMovimentos4, num4);
 
 return 0;
-    
+
 }
